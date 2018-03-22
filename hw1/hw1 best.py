e650b0c2e9e55256094826f9e383a31cdc8e4af7
Steve <steve@Stevede-MacBook-Pro.local>
2018-03-22 14:34:18 +0800

    20180322

---
+++ hw1/hw1 best.py
@@ -0,0 +1,59 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+import sys
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+
+# In[2]:
+
+
+#load csv file
+def load_data(filename):
+    return pd.read_csv(filename, encoding= 'big5')
+
+def Get_Result_Scaling(w,input_data,scale):
+    result = []
+    for i in range(len(input_data)):
+        f = np.array(input_data[i][1],dtype=float)
+        for j in range(len(input_data[i][1])):
+            if scale[j][0] != scale[j][1]:
+                f[j] = (f[j]-scale[j][1])/(scale[j][0] - scale[j][1])
+        result.append([input_data[i][0],np.dot(f,w)])
+    return result
+
+
+# In[6]:
+
+
+test_data = load_data(sys.argv[1])
+#in an hour
+input_data = []
+tmp = [1.0] + [list(test_data.columns)[-1]]
+for i in range(17):
+    tmp += [list(test_data.iloc[i])[-1]]
+tmp = [0 if x == 'NR' else x for x in tmp]
+input_data.append(['id_0',tmp])
+for j in range(1,260):
+    tmp = [1.0]
+    for i in range(18*j-1,18*j+17):
+        tmp += [list(test_data.iloc[i])[-1]]
+    tmp = [0 if x == 'NR' else x for x in tmp]
+    input_data.append([test_data.iloc[18*j-1][0], tmp])
+
+
+# In[7]:
+
+
+filename = sys.argv[2]
+w = np.load('best_model.npy')
+scale = np.load('best_scale.npy')
+result = Get_Result_Scaling(w,input_data,scale)
+export_df = pd.DataFrame(result,columns=['id','value'])
+export_df.to_csv(filename,sep=',',encoding='utf-8',index=False)
+
